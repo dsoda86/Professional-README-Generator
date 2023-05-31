846fb896b2f01defcc0060a3f3b851990d6a7213
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2023-05-30 21:53:24 -0700

    Added function to add license badge. Added more questions to better fill out the README.md file.

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateReadme = ({ title, description, installation, usage, contributing, tests, license, github, email }) => {
-    return `
-    `
-}
+function createLicenseBadge(license) {
+    if (license !== "none") {
+      return`[![Github License](https://img.shields.io/badge/License-${license}-blue.svg)](https://choosealicense.com/licenses/)`;
+    }else
+      return "";
+  };
+
+const generateReadme = ({ title, description, link, screenshot, demo, languages, installation, usage, contributors, testing, license, github, email }) => {
+    return `# ${title} 
+${createLicenseBadge(license)}
+## Description
+${description}
+## Deployed Application Link
+${link}
+## Table of Contents
+* [Screenshot](#screenshot)
+* [Demo](#demo)
+* [Languages & Technologies](#languagesandtechnologies)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+* [License](#license)
+ 
+## Screenshot
+![alt-text](${screenshot})
+## Demo
+![alt-text]${demo}
+## Languages & Technologies Used
+${languages}
+## Installation
+${installation}
+## Usage
+${usage}
+## Contributors
+${contributors}
+## Testing
+${testing}
+## Questions
+Check out my work at [github/${github}](https://github.com/${github}).
+Please send your questions to  [${email}](mailto:${email}?subject=[GitHub]%20Dev%20Connect).
+## License
+${license}
+`;
+};
+    
 
 const main = async() => {
     const answers = await inquirer
@@ -21,30 +63,51 @@ const main = async() => {
         },
         {
           type: "input",
+          name: "link",
+          message: "Please provide the deployed application URL.",
+        },
+        {
+          type: "input",
+          name: "screenshot",
+          message: "Please provide a link to any screenshots of the application.",
+        },
+        {
+          type: "input",
+          name: "demo",
+          message: "Please provide a link to a demo gif or video of the application.",
+        },
+        {
+          type: "input",
           name: "installation",
           message: "How do you install your application?",
         },
         {
-          type: "checkbox",
+          type: "input ",
           name: "usage",
+          message: "How do you use this application? Instructions to include in the usage section?",
+            
+        },
+        {
+          type: "input ",
+          name: "languages",
           message: "What languages or technologies are used with this application?",
-          choices: ["HTML", "CSS", "JavaScript", "Bootstrap", "Materialize", "Node.js", "React"]
+          
         },
         {
-          type: "confirm",
-          name: "contributing",
-          message: "Would you like to let others be able contribute?",
+          type: "input",
+          name: "contributors",
+          message: "List any contributors to the project here.",
         },
         {
-          type: "confirm",
-          name: "tests",
-          message: "Are there any tests that can be run for this application?",
+          type: "input",
+          name: "testing",
+          message: "Add any tests you want to be able to run here.",
         },
         {
           type: "checkbox",
           name: "license",
           message: "What type of license would you like to use for this project?",
-          choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"]
+          choices: ["MIT", "APACHE 2.0", "Boost 1.0", "MPL 2.0", "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "none"]
         },
         {
           type: 'input',
@@ -54,7 +117,7 @@ const main = async() => {
         {
           type: 'input',
           name: 'email',
-          message: 'Enter your contact email address.',
+          message: 'Enter your email address.',
         },
       ]);
   
